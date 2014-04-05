97ae021b9ce4fed37173130d24517dd70ff37aae
Patrick Davey <patrick.davey@resolvedigital.co.nz>
2014-04-05 14:32:28 +1300

    Correctly displaying a list of the days

--- Snowpool/Controllers/SPLSelectDateViewController.m
+++ Snowpool/Controllers/SPLSelectDateViewController.m
@@ -14,22 +14,36 @@
 
 @implementation SPLSelectDateViewController
 
+NSInteger const NumberOfDaysToShow = 10;
 
 #pragma mark -
 #pragma mark UITableView methods
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return 7;
+    return NumberOfDaysToShow;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
     static NSString *CellIdentifier = @"SelectDateCell";
     
+    NSDate *date = [NSDate date];
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+
+    NSDateComponents *dayComponent = [[NSDateComponents alloc] init];
+    dayComponent.day = indexPath.row;
     
-    cell.textLabel.text = @"hello";
+    NSCalendar *theCalendar = [NSCalendar currentCalendar];
+    date = [theCalendar dateByAddingComponents:dayComponent toDate:date options:0];
+    
+    //now date contains the offset from today that we want, format it into the string we want
+    
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc]init];
+    
+    [dateFormatter setDateFormat:@"d-MMM-YYYY"];
+    
+    cell.textLabel.text = [dateFormatter stringFromDate:date];
     
     return cell;
 }
